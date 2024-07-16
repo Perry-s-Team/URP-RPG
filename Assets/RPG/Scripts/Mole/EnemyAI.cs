dee4bb1a17f9c15c8936d641553ace9f488d4222
9ikii5ec <niiitakirkizh@gmail.com>
2024-07-16 21:37:52 +0300

    Update EnemyAi

--- Assets/RPG/Scripts/Mole/EnemyAI.cs
+++ Assets/RPG/Scripts/Mole/EnemyAI.cs
@@ -4,21 +4,44 @@ using RPG.Utils;
 
 public class EnemyAI : MonoBehaviour
 {
+    //[Header("EnemyProperty")]
+    
+
+    [Header("EnemyNavigation")]
     [SerializeField] private State startingState;
     [SerializeField] private float roamingDistanceMax = 7f;
     [SerializeField] private float roamingDistanceMin = 3f;
     [SerializeField] private float roamingTimerMax = 2f;
 
+    [Header("EnemyChasing")]
+    [SerializeField] private bool _isChasingEnemy = false;
+    [SerializeField] private float _chasingDisnance = 4f;
+    [SerializeField] private float _chasingSpeedMultiplayer = 2f;
+
     private NavMeshAgent _navMeshAgent;
-    private State _state;
-    private float _roamingTime;
+    private State _currentrState;
+    private float _roamingTimer;
     private Vector3 _roamingPosition;
     private Vector3 _startingPosition;
+    private float _roamingSpeed;
+    private float _chasingSpeed;
+
+    public bool IsRunning()
+    {
+        if (_navMeshAgent.velocity == Vector3.zero)
+            return false;
+        else
+            return true;
+    }
+
 
     private enum State
     {
         Idle,
-        Roaming
+        Roaming,
+        Chasing,
+        Attacking,
+        Death
     }
 
     private void Start()
@@ -31,23 +54,37 @@ public class EnemyAI : MonoBehaviour
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.updateRotation = false;
         _navMeshAgent.updateUpAxis = false;
-        _state = startingState;
+        _currentrState = startingState;
+        _roamingSpeed = _navMeshAgent.speed;
+        _chasingSpeed = _navMeshAgent.speed * _chasingSpeedMultiplayer;
     }
-
     private void Update()
     {
-        switch (_state)
+
+    }
+    private void StateHandler()
+    {
+        switch (_currentrState)
         {
             default:
             case State.Idle:
                 break;
             case State.Roaming:
-                _roamingTime -= Time.deltaTime;
-                if (_roamingTime < 0)
+                _roamingTimer -= Time.deltaTime;
+                if (_roamingTimer < 0)
                 {
                     Roaming();
-                    _roamingTime = roamingTimerMax;
+                    _roamingTimer = roamingTimerMax;
                 }
+                CheckCurrentState();
+                break;
+            case State.Chasing:
+                ChasingTarget();
+                CheckCurrentState();
+                break;
+            case State.Attacking:
+                break;
+            case State.Death:
                 break;
         }
     }
@@ -58,6 +95,42 @@ public class EnemyAI : MonoBehaviour
         _navMeshAgent.SetDestination(_roamingPosition);
         ChangeFacingDuration(_startingPosition, _roamingPosition);
     }
+
+    private void ChasingTarget()
+    {
+        _navMeshAgent.SetDestination(Movement.Instance.transform.position);
+    }
+
+    private void CheckCurrentState()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Movement.Instance.transform.position);
+        State newState = State.Roaming;
+
+        if (_isChasingEnemy)
+        {
+            if (distanceToPlayer <= _chasingDisnance)
+            {
+                newState = State.Chasing;
+            }
+        }
+
+        if (newState != _currentrState)
+        {
+            if (newState == State.Chasing)
+            {
+                _navMeshAgent.ResetPath();
+                _navMeshAgent.speed = _chasingSpeed;
+            }
+            else if (newState == State.Roaming) {
+            _roamingTimer = 0f;
+            _navMeshAgent.speed = _roamingSpeed; 
+            }
+
+            _currentrState = newState;
+        }
+
+    }
+
     private Vector3 GetRoamingPosition()
     {
         return _startingPosition + Utils.GetRandomDir() * UnityEngine.Random.Range(roamingDistanceMin, roamingDistanceMax);
@@ -68,6 +141,6 @@ public class EnemyAI : MonoBehaviour
         if (soursePosition.x > targetPosition.x)
             transform.rotation = Quaternion.Euler(0, -180, 0);
         else
-            transform.rotation = Quaternion.Euler(0,0,0);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
